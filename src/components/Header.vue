e6d8930676043d4263e582e6d9d4b8363f4c242a
Sdager <129157098+Sdager@users.noreply.github.com>
2024-09-17 17:30:56 +0300

    test

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template> <div class="container">
+    <div class="row ">
+        <div class="col-4 ">ds</div>
+        <div class="col-4">ds</div>
+        <div class="col-4">ds</div>
+    </div>
+    </div>
+</template>
+
+<style>
+.container{
+    padding: 0px;
+    margin: 0px;
+    width: 100vh;
+}
+</style>
