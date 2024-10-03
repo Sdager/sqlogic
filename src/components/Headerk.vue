edce52b9d60ab6fdd89e8498130fb613627413b2
Sdager <sdagerok@gmail.com>
2024-10-03 23:02:07 +0300

    adaptive start

--- src/components/Headerk.vue
+++ src/components/Headerk.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="row custom_container">
-    <div class="col-2 ">
+    <div class="col-2 col-md-2 col-sm-3 col-xs-4">
       <ol>
         <li>Контакты</li>
         <li>Услуги</li>
       </ol>
     </div>
-    <div class="col-9 d-flex justify-content-center align-items-center"><img src="/src/assets/logo.svg" alt=""></div>
-    <div class="right col-1 d-flex justify-content-start">О компании</div>
+    <div class="col-9 col-md-8 col-sm-6 col-xs-3 d-flex justify-content-center align-items-center"><img src="/src/assets/logo.svg" alt=""></div>
+    <div class="right col-1  col-md-2 col-sm-3  col-xs-5 d-flex justify-content-start">О компании</div>
   </div>
 </template>
 
@@ -32,7 +32,7 @@ margin-right:6em ;
 }
 img{
   cursor: pointer;
-  height: 100%;
+  height: 60px;
 }
 ol{
   cursor: pointer;
@@ -42,4 +42,82 @@ ol{
     flex-direction: row;
     gap: 1.7em;
 }
+@media (max-width: 1500px) {
+  .custom_container {
+  padding: 0px;
+margin-left: 6em;
+margin-right:8em ;
+  user-select: none; 
+  
+
+}
+.col-2, .col-1 {
+  margin-top: 0.6em;
+}
+.right{
+  padding-right: 0.2em;
+}
+img{
+  cursor: pointer;
+  height: 60px;
+}
+ol{
+  cursor: pointer;
+  
+    list-style-type: none;
+    display: flex;
+    flex-direction: row;
+    gap: 1.7em;
+}
+ }
+ @media (max-width: 900px) {
+  .custom_container {
+  padding: 0px;
+margin-left: 1em;
+margin-right:0em ;
+  user-select: none; 
+  
+
+}
+.col-2, .col-1 {
+  margin-top: 0.6em;
+}
+
+img{
+  cursor: pointer;
+  height: 50px;
+}
+ol{
+  cursor: pointer;
+  
+    list-style-type: none;
+    display: flex;
+    flex-direction: row;
+    gap: 1.7em;
+}
+ }
+ @media (max-width: 590px) {
+  .custom_container {
+  padding: 0px;
+
+
+  user-select: none; 
+  
+
+}
+
+
+img{
+  cursor: pointer;
+  height: 40px;
+}
+ol{
+  cursor: pointer;
+  
+    list-style-type: none;
+    display: flex;
+    flex-direction: row;
+    gap: 1.7em;
+}
+ }
 </style>
--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -7,7 +7,7 @@
   <div>
     <div class="card row">
       
-        <div class="left col-6">
+        <div class="left col-6 ">
           <div class="up_text">SQLogic — это <br> эффективный <br> аутсорсинг</div>
           <div class="txt">
           <div class="bottom_text">IT-отдел по цене одного <br> специалиста</div>
@@ -15,15 +15,15 @@
         </div>
       </div>       
 
-        <div class="col-6 ">
+        <div class="col-6 bot">
             <div class="time"></div>
             <div class="plusi row">
-                <div class="icons col-1">
+                <div class="icons col-2">
                     <img src="/src/assets/res.svg" alt="">
                     <img src="/src/assets/kach.svg" alt="">
                     <img src="/src/assets/stab.svg" alt="">
                 </div>
-                <ol  class="col-11">
+                <ol  class="col-10">
                     <li>стабильность <br>наши решения играют в долгую</li>
                     <li>качество <br>высокие стандарты обслуживания</li>
                     <li>результат <br>помогаем вашему бизнесу расти</li>
@@ -42,14 +42,14 @@
     margin: 0px;
 }
 img{
-    height: 40px
+    height: 40px;
 }
 .card {
   display: flex;
   justify-content: space-between;
   align-items: center;
   background-color: #c9d1ff;
-  height: 53vh;
+  height: 65vh;
   margin-left: 9em;
   margin-right: 9em;
   margin-top: 1em;
@@ -76,7 +76,7 @@ img{
   display: flex;
   flex-direction: column;
   align-items: flex-end; 
-  gap: 2em;
+  gap: 2.5em;
  
   padding-right: 1em;
   min-width: 300px; 
@@ -107,7 +107,7 @@ img{
 
 .bottom_text {
     font-size: 3vw; 
-    padding-right: 2.1em;
+    padding-right: 4em;
 }
 
 button {
@@ -117,8 +117,8 @@ button {
   border: none;
   height: 2.4em;
   font-size: 16px;
-  width: 60%;
-  margin-top:2em ;
+  width: 100%;
+  margin-top:1em ;
 }
 
 ol {
@@ -129,7 +129,7 @@ ol {
 }
 
 /* Медиазапрос для широких экранов (больше 1200px) */
-@media (min-width: 1200px) {
+@media (min-width: 1500px) {
   .up_text {
     font-size: 64px; 
   }
@@ -138,65 +138,186 @@ ol {
     font-size: 28px; 
   }
   button{
-    width: 50%;
+    width: 100%;
 
   }
   .left{
     gap: 3em;
   }
 }
+@media (max-width: 1500px) {
+  .up_text {
+    font-size: 45px; 
+  }
+  .bottom_text{
+    font-size: 20px;
+  }
+  .card{
+    height: 50vh;
 
+  }
+ 
+}
+
+
+@media (max-width: 1150px) {
+  .up_text {
+    font-size: 30px; 
+  }
+  .bottom_text{
+    font-size: 13px;
+  }
+  .card{
+    height: 40vh;
+
+  }
+  img{
+    gap: 2em;
+    height: 50;
+  }
+  ol {
+font-size: 13px;
+  gap: 3.5em;
+  list-style-type: none;
+}
+}
 /* Медиазапросы для адаптивности */
-@media (max-width: 768px) {
+@media (max-width: 890px) {
+  .up_text {
+    font-size: 37px; 
+  }
+  .bottom_text{
+    font-size: 16px;  width: 100%;padding:0px ;
+  
+  }
   .card {
+    padding: 1em;
+    display: flex  !important;
     flex-direction: column; /* Блоки друг под другом на маленьких экранах */
     height: auto; /* Автоматическая высота */
     margin-left: 1em;
     margin-right: 1em;
+gap: 2em;
   }
 
-  .left {
-    align-items: center; /* Центрируем контент на маленьких экранах */
-    text-align: center;
-   
-    border-bottom: 2px solid grey; /* Добавляем черту снизу */
-    padding-right: 0;
-    padding-bottom: 1em;
-  }
-
-  .up_text {
-    font-size: 8vw; /* Увеличиваем относительно ширины экрана */
-  }
-
-  .bottom_text {
-    font-size: 5vw;
-    padding-right: 0;
-  }
-
-  button {
-    width: 80%;
-    font-size: 14px;
-  }
-
-  .col-6 {
-    align-items: center;
-    text-align: center;
-  }
+  .icons{
+    padding: 0px;
+    margin: 0px;
+}
+img{
+    height: 30px;
 }
 
-@media (max-width: 480px) {
+.txt{
+width: 100%;
+
+}
+button{
+  width: 100%;
+}
+
+.left {
+  
+display: flex;
+justify-content: start;
+  
+ 
+ 
+  
+}
+.icons{
+  gap: 2.5em;
+}
+
+ol{
+  gap: 2em;
+}
+
+
+.card::before {
+  content: none; 
+  position: absolute;
+  top: 0px; 
+  bottom: 0px; 
+  left: 0%; 
+  transform: none; 
+  width: 0px;
+  background-color:none; 
+}
+
+.up_text{
+  align-content: start;
+  text-align: start;
+  display: flex;
+  height: 100%;
+}
+
+}
+
+@media (max-width: 460px) {
   .up_text {
-    font-size: 10vw; /* Для маленьких экранов увеличиваем масштаб */
+    font-size: 38px; /* Для маленьких экранов увеличиваем масштаб */
   }
 
   .bottom_text {
-    font-size: 6vw;
+    font-size: 4vw;
   }
 
   button {
     width: 100%; /* Увеличиваем кнопку на маленьких экранах */
     font-size: 12px; /* Уменьшаем размер текста на кнопке */
   }
+  ol{
+  gap: 1.2em;
+  width: 70%;
+
+}
+.icons{
+  gap: 1.5em;
+  padding-top: 10px;
+ margin-left:4em ;
+  width: 20px;
+}
+.bot{
+text-align: start;
+width: 100%;
+display: flex;
+flex-direction: center;
+}
+.plusi{margin: 0;}
+}
+
+@media (max-width: 480px) {
+  .up_text {
+    font-size: 38px; /* Для маленьких экранов увеличиваем масштаб */
+  }
+
+  .bottom_text {
+    font-size: 4vw;
+  }
+
+  button {
+    width: 100%; /* Увеличиваем кнопку на маленьких экранах */
+    font-size: 12px; /* Уменьшаем размер текста на кнопке */
+  }
+  ol{
+  gap: 1.2em;
+  width: 70%;
+
+}
+.icons{
+  gap: 1.5em;
+  padding-top: 10px;
+ margin-left:0em ;
+  width: 20px;
+}
+.bot{
+text-align: start;
+width: 100%;
+display: flex;
+flex-direction: center;
+}
+.plusi{margin: 0;}
 }
 
 
