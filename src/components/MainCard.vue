1e2ac7c2e3eb49fdaa7507c50414fd64c394a7a2
Sdager <sdagerok@gmail.com>
2024-10-30 23:20:19 +0300

    modal

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -1,48 +1,77 @@
-
-<script></script>
-
-
+<script>
+import Modal from "./Modal.vue";
+export default {
+    name: 'app',
+    components: {
+      Modal,
+    },
+    data () {
+      return {
+        isModalVisible: false,
+      };
+    },
+    methods: {
+      showModal() {
+        this.isModalVisible = true;
+      },
+      closeModal() {
+        this.isModalVisible = false;
+      }
+    },
+  };
+</script>
 
 <template>
   <div>
     <div class="card row">
-      
-        <div class="left col-6 ">
-          <div class="up_text">SQLogic — это <br> эффективный <br> аутсорсинг</div>
-          <div class="txt">
-          <div class="bottom_text">IT-отдел по цене одного <br> специалиста</div>
-         <button>Связаться с нами!</button>
+      <div class="left col-6">
+        <div class="up_text">
+          SQLogic — это <br />
+          эффективный <br />
+          аутсорсинг
         </div>
-      </div>       
-
-        <div class="col-6 bot">
-            <div class="time"></div>
-            <div class="plusi row">
-                <div class="icons col-2">
-                    <img src="/src/assets/res.svg" alt="">
-                    <img src="/src/assets/kach.svg" alt="">
-                    <img src="/src/assets/stab.svg" alt="">
-                </div>
-                <ol  class="col-10">
-                    <li>стабильность <br>наши решения играют в долгую</li>
-                    <li>качество <br>высокие стандарты обслуживания</li>
-                    <li>результат <br>помогаем вашему бизнесу расти</li>
-                </ol>
-            </div>
-           <div class="icon_bottom"></div>
-
+        <div class="txt">
+          <div class="bottom_text">
+            IT-отдел по цене одного <br />
+            специалиста
+          </div>
+          <button type="button" class="btn" @click="showModal">
+            Связаться с нами!
+          </button>
+          <Modal
+      v-show="isModalVisible"
+      @close="closeModal"
+    />
         </div>
+
+      </div>
+
+      <div class="col-6 bot">
+        <div class="time"></div>
+        <div class="plusi row">
+          <div class="icons col-2">
+            <img src="/src/assets/res.svg" alt="" />
+            <img src="/src/assets/kach.svg" alt="" />
+            <img src="/src/assets/stab.svg" alt="" />
+          </div>
+          <ol class="col-10">
+            <li>стабильность <br />наши решения играют в долгую</li>
+            <li>качество <br />высокие стандарты обслуживания</li>
+            <li>результат <br />помогаем вашему бизнесу расти</li>
+          </ol>
+        </div>
+        <div class="icon_bottom"></div>
       </div>
     </div>
-
+  </div>
 </template>
 <style scoped>
-.icons{
-    padding: 0px;
-    margin: 0px;
+.icons {
+  padding: 0px;
+  margin: 0px;
 }
-img{
-    height: 40px;
+img {
+  height: 40px;
 }
 .card {
   display: flex;
@@ -61,33 +90,32 @@ img{
 }
 
 .card::before {
-  content: ''; 
+  content: "";
   position: absolute;
-  top: 75px; 
-  bottom: 75px; 
-  left: 50%; 
-  transform: translateX(-50%); 
+  top: 75px;
+  bottom: 75px;
+  left: 50%;
+  transform: translateX(-50%);
   width: 1px;
-  background-color: rgb(129, 129, 129); 
+  background-color: rgb(129, 129, 129);
 }
-
 
 .left {
   display: flex;
   flex-direction: column;
-  align-items: flex-end; 
+  align-items: flex-end;
   gap: 2.5em;
- 
+
   padding-right: 1em;
-  min-width: 300px; 
+  min-width: 300px;
   max-width: 100%;
-  padding-right:2em ;
+  padding-right: 2em;
 }
 
 .col-6 {
   display: flex;
   flex-direction: column;
-  justify-content: center; 
+  justify-content: center;
   height: 100%;
 }
 
@@ -100,14 +128,14 @@ img{
 }
 
 .up_text {
-    line-height: 100%;
-    font-size: 6vw; 
-    font-weight: bold;
+  line-height: 100%;
+  font-size: 6vw;
+  font-weight: bold;
 }
 
 .bottom_text {
-    font-size: 3vw; 
-    padding-right: 4em;
+  font-size: 3vw;
+  padding-right: 4em;
 }
 
 button {
@@ -118,7 +146,7 @@ button {
   height: 2.4em;
   font-size: 16px;
   width: 100%;
-  margin-top:1em ;
+  margin-top: 1em;
 }
 
 ol {
@@ -131,146 +159,133 @@ ol {
 /* Медиазапрос для широких экранов (больше 1200px) */
 @media (min-width: 1500px) {
   .up_text {
-    font-size: 64px; 
+    font-size: 64px;
   }
-img{
-  height: 4em;
-  
-}
-.icons{
-  margin-left: 2em;
-  gap: 4.5em;
-  width: 5em;
-}
-li{
-  font-size: 18px;
-}
-ol{
-  gap: 4.9em;
-  padding-top: 0.5em;
-}
+  img {
+    height: 4em;
+  }
+  .icons {
+    margin-left: 2em;
+    gap: 4.5em;
+    width: 5em;
+  }
+  li {
+    font-size: 18px;
+  }
+  ol {
+    gap: 4.9em;
+    padding-top: 0.5em;
+  }
   .bottom_text {
-    font-size: 28px; 
+    font-size: 28px;
   }
-  button{
+  button {
     width: 100%;
     height: 3em;
-    font-size:20px ;
-
+    font-size: 20px;
   }
-  .left{
+  .left {
     gap: 3em;
   }
 }
 @media (max-width: 1500px) {
   .up_text {
-    font-size: 45px; 
+    font-size: 45px;
   }
-  .bottom_text{
+  .bottom_text {
     font-size: 20px;
   }
-  .card{
+  .card {
     height: 50vh;
-
   }
- 
 }
-
 
 @media (max-width: 1150px) {
   .up_text {
-    font-size: 30px; 
+    font-size: 30px;
   }
-  .bottom_text{
+  .bottom_text {
     font-size: 13px;
   }
-  .card{
+  .card {
     height: 40vh;
-    margin-right:7.5em ;
+    margin-right: 7.5em;
   }
-  img{
+  img {
     gap: 2em;
     height: 50;
   }
   ol {
-font-size: 13px;
-  gap: 3.5em;
-  list-style-type: none;
-}
+    font-size: 13px;
+    gap: 3.5em;
+    list-style-type: none;
+  }
 }
 /* Медиазапросы для адаптивности */
 @media (max-width: 890px) {
   .up_text {
-    font-size: 37px; 
+    font-size: 37px;
   }
-  .bottom_text{
-    font-size: 16px;  width: 100%;padding:0px ;
-  
+  .bottom_text {
+    font-size: 16px;
+    width: 100%;
+    padding: 0px;
   }
   .card {
     padding: 1em;
-    display: flex  !important;
+    display: flex !important;
     flex-direction: column; /* Блоки друг под другом на маленьких экранах */
     height: auto; /* Автоматическая высота */
     margin-left: 9em;
     margin-right: 6.2em;
-gap: 2em;
+    gap: 2em;
   }
 
-  .icons{
+  .icons {
     padding: 0px;
     margin: 0px;
-}
-img{
+  }
+  img {
     height: 30px;
-}
+  }
 
-.txt{
-width: 100%;
+  .txt {
+    width: 100%;
+  }
+  button {
+    width: 100%;
+  }
 
-}
-button{
-  width: 100%;
-}
+  .left {
+    display: flex;
+    justify-content: start;
+  }
+  .icons {
+    gap: 2.5em;
+  }
 
-.left {
-  
-display: flex;
-justify-content: start;
-  
- 
- 
-  
-}
-.icons{
-  gap: 2.5em;
-}
+  ol {
+    gap: 2em;
+  }
 
-ol{
-  gap: 2em;
+  .card::before {
+    content: none;
+    position: absolute;
+    top: 0px;
+    bottom: 0px;
+    left: 0%;
+    transform: none;
+    width: 0px;
+    background-color: none;
+  }
+
+  .up_text {
+    align-content: start;
+    text-align: start;
+    display: flex;
+    height: 100%;
+  }
 }
-
-
-.card::before {
-  content: none; 
-  position: absolute;
-  top: 0px; 
-  bottom: 0px; 
-  left: 0%; 
-  transform: none; 
-  width: 0px;
-  background-color:none; 
-}
-
-.up_text{
-  align-content: start;
-  text-align: start;
-  display: flex;
-  height: 100%;
-}
-
-}
-
 
 @media (max-width: 580px) {
   .up_text {
@@ -285,29 +300,28 @@ ol{
     width: 100%; /* Увеличиваем кнопку на маленьких экранах */
     font-size: 12px; /* Уменьшаем размер текста на кнопке */
   }
-  ol{
-  gap: 1.2em;
-  width: 70%;
-
+  ol {
+    gap: 1.2em;
+    width: 70%;
+  }
+  .icons {
+    gap: 1.5em;
+    padding-top: 10px;
+    margin-left: 0em;
+    width: 20px;
+  }
+  .card {
+    margin-left: 1em;
+    margin-right: 1em;
+  }
+  .bot {
+    text-align: start;
+    width: 100%;
+    display: flex;
+    flex-direction: center;
+  }
+  .plusi {
+    margin: 0;
+  }
 }
-.icons{
-  gap: 1.5em;
-  padding-top: 10px;
- margin-left:0em ;
-  width: 20px;
-}
-.card{
-  margin-left:1em ;
-  margin-right: 1em;
-}
-.bot{
-text-align: start;
-width: 100%;
-display: flex;
-flex-direction: center;
-}
-.plusi{margin: 0;}
-}
-
-
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+  name: "modal",
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+<template>
+  <div class="modal-backdrop">
+    <div class="modal">
+      <button type="button" class="btn-closes" @click="close">x</button>
+    </div>
+  </div>
+</template>
+<style>
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex; 
+  justify-content: center;
+  align-items: center;
+}
+
+.modal {
+  background: #ffffff;
+  box-shadow: 2px 2px 20px 1px;
+
+  display: flex;
+  flex-direction: column;
+
+  width: 300px;
+  height: 300px;
+  position: relative; 
+  border-radius: 8px; 
+}
+
+.btn-closes {
+  border: none;
+  justify-content: end;
+  cursor: pointer;
+  font-weight: bold;
+  color: #4aae9b;
+  text-align: end;
+  background: white;
+  width: 10px;
+}
+</style>
